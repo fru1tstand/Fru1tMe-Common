ec6d935609607202ddb2456d5907bffa0e4531f7
Kodlee Yin <kodlee@kodleeshare.net>
2015-12-21 22:57:08 -0800

    Added lang class

---
+++ base/Lang.php
@@ -0,0 +1,24 @@
+<?php
+namespace common\base;
+
+/**
+ * Contains methods and constructs that extend the core functionality of PHP.
+ * @package common\base
+ */
+class Lang {
+	/**
+	 * Check if one or more variables are null. Returns true on the first encountered null variable.
+	 * Otherwise, returns true.
+	 *
+	 * @param mixed[] ...$vars
+	 * @return bool
+	 */
+	public static function isNull(mixed ...$vars): bool {
+		foreach ($vars as $var) {
+			if (is_null($var)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
