e5b4172f2f6a26b4f02bb4c6f50fd5c900f3e4fe
Kodlee Yin <kodlee@fru1t.me>
2017-03-23 19:50:21 -0700

    Created ReturnContext class

---
+++ language/ReturnContext.php
@@ -0,0 +1,35 @@
+<?php
+namespace me\fru1t\common\language;
+
+/**
+ * A simple php object that contains a boolean and string to denote method return success and a
+ * status message.
+ */
+class ReturnContext {
+  /** @var boolean */
+  private $didSucceed;
+  /** @var null|string */
+  private $message;
+
+  public function __construct(bool $didSucceed, ?string $message) {
+    $this->didSucceed = $didSucceed;
+    $this->message = $message;
+  }
+
+  /**
+   * Returns whether or not the call to the function succeeded.
+   * @return bool
+   */
+  public function didSucceed(): bool {
+    return $this->didSucceed;
+  }
+
+  /**
+   * Returns the call's context message. Always returns a string, even an empty string if the
+   * status wasn't given by the returning function.
+   * @return string
+   */
+  public function getMessage(): string {
+    return $this->message ?? '';
+  }
+}
